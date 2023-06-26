16f7150d0ada9611f059be128f280df73cd6f1de
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-06-26 17:18:39 +0900

    ✨ test #51: findAll 검증문 추가
    - 카테고리가 제대로 들어있는지 확인합니다.

--- be/src/test/java/com/kiosk/api/product/web/controller/ProductControllerTest.java
+++ be/src/test/java/com/kiosk/api/product/web/controller/ProductControllerTest.java
@@ -1,13 +1,19 @@
 package com.kiosk.api.product.web.controller;
 
+import static com.kiosk.api.product.domain.entity.CategoryType.*;
 import static java.nio.charset.StandardCharsets.UTF_8;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kiosk.api.product.domain.entity.Category;
+import com.kiosk.api.product.domain.entity.CategoryType;
 import com.kiosk.api.product.web.controller.dto.ProductCategoryResponse;
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 import org.assertj.core.api.SoftAssertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -39,7 +45,18 @@ class ProductControllerTest {
         List<ProductCategoryResponse> products = objectMapper.readValue(json, new TypeReference<>() {
         });
         SoftAssertions.assertSoftly(softAssertions -> {
+            // 카테고리 개수 테스트
             softAssertions.assertThat(products.size()).isEqualTo(5);
+
+            // 카테고리 테스트
+            List<String> categoryNames = products.stream()
+                .map(ProductCategoryResponse::getCategoryName)
+                .collect(Collectors.toUnmodifiableList());
+            softAssertions.assertThat(categoryNames.contains(COFFEE.name())).isTrue();
+            softAssertions.assertThat(categoryNames.contains(LATTE.name())).isTrue();
+            softAssertions.assertThat(categoryNames.contains(TEA.name())).isTrue();
+            softAssertions.assertThat(categoryNames.contains(JUICE.name())).isTrue();
+            softAssertions.assertThat(categoryNames.contains(SPARKLING.name())).isTrue();
         });
     }
 }
