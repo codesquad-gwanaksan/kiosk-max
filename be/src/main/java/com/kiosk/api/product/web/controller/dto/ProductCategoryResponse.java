03426bceaef4222c7c4178cd3a74c5b396f00407
YongHwan Kim <yonghwankim.dev@gmail.com>
2023-06-27 11:24:12 +0900

    [BE] 상품 테스트 코드 구현 (#60)
    * 🐛 fix #51: h2 의존성 중복 제거
    
    * ✨ test #51: findAll 테스트 코드 추가
    
    * ✨ test #51: products 테스트 코드 추가
    
    클라이언트에서 `/products` 호출시 응답받는 테스트 추가
    
    * ✨ chore #51: h2 추가
    
    테스트 코드에서 사용하기 위해서 추가하였습니다.
    
    * ✨ test #51: h2 스키마 및 샘플 데이터 스크립트 추가로 인한 테스트 코드 변경
    
    * ✨ test #51: h2 schema.sql, data.sql, h2 데이터 소스 추가
    
    - 프로파일은 test입니다.
    
    * ✨ test #51: findAll 검증문 추가
    
    - 카테고리가 제대로 들어있는지 확인합니다.
    
    * ✨ test #51: /products api 테스트 코드 수정
    
    * ✨ test #51: beforeEach 구현하여 중복되는 설정 및 데이터 초기화 제거
    
    * ✨ test #51: JdbcProductRepository가 DataJpaTest를 하도록 변경
    
    * ✨ test #51: assertAll 추가
    
    - assertAll 메소드 호출을 추가하여 중간에 테스트가 실패해도 모든 테스트를 실행할 수 있도록 합니다.
    
    * ✨ fix #51: 테스트코드에서만 사용하는 공백 생성자를 제거

--- be/src/main/java/com/kiosk/api/product/web/controller/dto/ProductCategoryResponse.java
+++ be/src/main/java/com/kiosk/api/product/web/controller/dto/ProductCategoryResponse.java
@@ -4,10 +4,13 @@ import java.util.List;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 @AllArgsConstructor
 @Builder
 @Getter
+@ToString
 public class ProductCategoryResponse {
 
     private String categoryName;
