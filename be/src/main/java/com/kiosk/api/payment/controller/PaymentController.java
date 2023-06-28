380df99bd3edcea204e9f2bc70159a84379ee9d4
Sully <2dcoder@naver.com>
2023-06-28 17:43:58 +0900

    ♻️ refactor(PaymentController) #50: 컨트롤러와 서비스 분리

--- be/src/main/java/com/kiosk/api/payment/controller/PaymentController.java
+++ be/src/main/java/com/kiosk/api/payment/controller/PaymentController.java
@@ -1,75 +1,27 @@
 package com.kiosk.api.payment.controller;
 
-import com.kiosk.api.order.domain.entity.OrderProduct;
-import com.kiosk.api.order.domain.repository.OrderProductRepository;
-import com.kiosk.api.payment.domain.dto.PaymentRequestDto.CartInDto;
 import com.kiosk.api.payment.domain.dto.PaymentRequestDto.PayByCardInDto;
 import com.kiosk.api.payment.domain.dto.PaymentRequestDto.PayByCashInDto;
-import com.kiosk.api.payment.domain.entity.Payment;
-import com.kiosk.api.payment.domain.repository.PaymentRepository;
-import org.springframework.transaction.annotation.Transactional;
+import com.kiosk.api.payment.service.PaymentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
 import java.util.Map;
 
-@Transactional
 @RestController
+@RequiredArgsConstructor
 public class PaymentController {
-    private final PaymentRepository paymentRepository;
-    private final OrderProductRepository orderProductRepository;
-
-    public PaymentController(final PaymentRepository paymentRepository, final OrderProductRepository orderProductRepository) {
-        this.paymentRepository = paymentRepository;
-        this.orderProductRepository = orderProductRepository;
-    }
+    private final PaymentService paymentService;
 
     @PostMapping("/api/payment/cash")
     public Map<String, Long> payByCash(@RequestBody final PayByCashInDto payByCashInDto) {
-        // TODO: 주문 만들기
-        Long orderId = 0L;
-
-        // orderId로 orderProducts 완성해서 모두 저장하기
-        List<CartInDto> orderProducts = payByCashInDto.getOrderProducts();
-        saveOrderProductsWithOrderId(orderId, orderProducts);
-
-        // 총액이랑 받은 돈으로 거스름 돈 계산해서 payment 저장하기
-        Payment payment = payByCashInDto.toEntity(orderId);
-        paymentRepository.save(payment);
-
-        // TODO: 영수증 저장하기
-
-        // TODO: 무늬만 DTO -> 나중에 공용 ResponseDTO 에 넣기
-        Map<String, Long> payByCashOutDto = Map.of("orderId", orderId);
-        return payByCashOutDto;
+        return paymentService.createPaymentByCash(payByCashInDto);
     }
 
     @PostMapping("/api/payment/card")
     public Map<String, Long> payByCard(@RequestBody final PayByCardInDto payByCardInDto) {
-        // TODO: 주문 만들기
-        Long orderId = 0L;
-
-        // orderId로 orderProducts 완성해서 모두 저장하기
-        List<CartInDto> orderProducts = payByCardInDto.getOrderProducts();
-        saveOrderProductsWithOrderId(orderId, orderProducts);
-
-        // TODO: 3 ~ 7초 휴식, 카드 결제 실패 정하기
-        Payment payment = payByCardInDto.toEntity(orderId);
-        paymentRepository.save(payment);
-
-        // TODO: 영수증 저장하기
-
-        // TODO: 무늬만 DTO -> 나중에 공용 ResponseDTO 에 넣기
-        Map<String, Long> payByCardOutDto = Map.of("orderId", orderId);
-        return payByCardOutDto;
-    }
-
-    private void saveOrderProductsWithOrderId(final Long orderId, final List<CartInDto> orderProducts) {
-        for (CartInDto orderProduct : orderProducts) {
-            OrderProduct completedOrderProduct = orderProduct.toEntity(orderId);
-            orderProductRepository.save(completedOrderProduct);
-        }
+        return paymentService.createPaymentByCard(payByCardInDto);
     }
 }
