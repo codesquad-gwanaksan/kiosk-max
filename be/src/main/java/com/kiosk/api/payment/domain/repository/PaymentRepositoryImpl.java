933d2ed6e3c738eda5a6f244902e77b9b47d11ab
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-06-28 21:42:12 +0900

    🐛 fix #51: 결제 저장 코드 수정
    - enum 타입이 저장되도록 변경

--- be/src/main/java/com/kiosk/api/payment/domain/repository/PaymentRepositoryImpl.java
+++ be/src/main/java/com/kiosk/api/payment/domain/repository/PaymentRepositoryImpl.java
@@ -2,6 +2,7 @@ package com.kiosk.api.payment.domain.repository;
 
 import com.kiosk.api.payment.domain.entity.Payment;
 import com.kiosk.api.payment.domain.entity.PaymentMethod;
+import java.sql.Types;
 import org.springframework.jdbc.core.RowMapper;
 import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -16,6 +17,7 @@ import java.util.Optional;
 
 @Repository
 public class PaymentRepositoryImpl implements PaymentRepository {
+
     // 결제 -> order id, total amount, received amount, change, method
     private final NamedParameterJdbcTemplate template;
 
@@ -25,10 +27,12 @@ public class PaymentRepositoryImpl implements PaymentRepository {
 
     @Override
     public Long save(Payment payment) {
-        String sql = "INSERT INTO payment (order_id, payment_total_price, payment_received_price, payment_remained_price, payment_method) "
+        String sql =
+            "INSERT INTO payment (order_id, payment_total_price, payment_received_price, payment_remained_price, payment_method) "
                 + "values (:orderId, :totalPrice, :receivedPrice, :remainedPrice, :method)";
 
-        SqlParameterSource param = new BeanPropertySqlParameterSource(payment);
+        BeanPropertySqlParameterSource param = getBeanPropertySqlParameterSourcePayment(payment);
+        param.registerSqlType("payment_method", Types.VARCHAR);
         KeyHolder keyHolder = new GeneratedKeyHolder();
 
         template.update(sql, param, keyHolder);
@@ -36,28 +40,50 @@ public class PaymentRepositoryImpl implements PaymentRepository {
         return Objects.requireNonNull(keyHolder.getKey()).longValue();
     }
 
+    private BeanPropertySqlParameterSource getBeanPropertySqlParameterSourcePayment(Payment payment) {
+        return new BeanPropertySqlParameterSource(payment) {
+            @Override
+            public Object getValue(String paramName) throws IllegalArgumentException {
+                Object value = super.getValue(paramName);
+                if (value instanceof Enum) {
+                    return value.toString();
+                }
+                return value;
+            }
+        };
+    }
+
+    @Override
+    public Optional<Payment> findBy(final Long paymentId) {
+        String sql =
+            "SELECT payment_id, order_id, payment_total_price, payment_received_price, payment_remained_price, payment_method "
+                + "FROM payment "
+                + "WHERE payment_id = :paymentId";
+
+        SqlParameterSource param = new MapSqlParameterSource("paymentId", paymentId);
+        return template.query(sql, param, rowMapper()).stream().findAny();
+    }
+
     // 1:1 이니까 1개만 가져오고 싶은데
     @Override
-    public Optional<Payment> findBy(Long orderId) {
-        String sql = "SELECT order_id, payment_total_price, payment_received_price, payment_remained_price, payment_method "
+    public Optional<Payment> findByOrderId(Long orderId) {
+        String sql =
+            "SELECT order_id, payment_total_price, payment_received_price, payment_remained_price, payment_method "
                 + "FROM payment "
                 + "WHERE order_id = :orderId";
 
         SqlParameterSource param = new MapSqlParameterSource("orderId", orderId);
-
-        // ResultSetExtractor 은 무엇?
-        Payment payment = template.queryForObject(sql, param, rowMapper());
-
-        return Optional.ofNullable(payment);
+        return template.query(sql, param, rowMapper()).stream().findAny();
     }
 
     private RowMapper<Payment> rowMapper() {
         return (rs, rn) -> Payment.builder()
-                .orderId(rs.getLong("order_id"))
-                .totalPrice(rs.getInt("payment_total_price"))
-                .receivedPrice(rs.getInt("payment_received_price"))
-                .remainedPrice(rs.getInt("payment_remained_price"))
-                .method(PaymentMethod.from(rs.getString("payment_method")))
-                .build();
+            .paymentId(rs.getLong("payment_id"))
+            .orderId(rs.getLong("order_id"))
+            .totalPrice(rs.getInt("payment_total_price"))
+            .receivedPrice(rs.getInt("payment_received_price"))
+            .remainedPrice(rs.getInt("payment_remained_price"))
+            .method(PaymentMethod.from(rs.getString("payment_method")))
+            .build();
     }
 }
