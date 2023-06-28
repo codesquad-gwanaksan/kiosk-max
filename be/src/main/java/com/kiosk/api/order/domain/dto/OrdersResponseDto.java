fcb257f5d25fc08963c4c9821daff41e9bdd9b42
jinny-l <jinny.l.dev@gmail.com>
2023-06-28 16:22:28 +0900

    ✨feat #50: 영수증 API 개발을 위한 OrdersResponseDto 구현

---
+++ be/src/main/java/com/kiosk/api/order/domain/dto/OrdersResponseDto.java
@@ -0,0 +1,11 @@
+package com.kiosk.api.order.domain.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
+public class OrdersResponseDto {
+
+    private String orderDateTime;
+}
