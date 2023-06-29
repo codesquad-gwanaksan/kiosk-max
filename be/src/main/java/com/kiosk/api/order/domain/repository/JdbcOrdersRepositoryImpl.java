db350a8bae6bbb1c780d0f7341f108fbfc391958
yonghwankim-dev <yonghwankim.dev@gmail.com>
2023-06-29 14:40:50 +0900

    🐛 fix #8: 주문 저장 로직 문제 해결
    - 쿼리에 들어갈 네임드 파라미터 설정값 추가

--- be/src/main/java/com/kiosk/api/order/domain/repository/JdbcOrdersRepositoryImpl.java
+++ be/src/main/java/com/kiosk/api/order/domain/repository/JdbcOrdersRepositoryImpl.java
@@ -1,21 +1,15 @@
 package com.kiosk.api.order.domain.repository;
 
-import com.kiosk.api.order.domain.entity.OrderProduct;
 import com.kiosk.api.order.domain.entity.Orders;
+import java.time.LocalDate;
+import java.time.ZonedDateTime;
+import java.util.Optional;
 import org.springframework.jdbc.core.RowMapper;
-import org.springframework.jdbc.core.namedparam.BeanPropertySqlParameterSource;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
 import org.springframework.jdbc.core.namedparam.SqlParameterSource;
-import org.springframework.jdbc.support.GeneratedKeyHolder;
-import org.springframework.jdbc.support.KeyHolder;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Repository;
-
-import java.time.LocalDate;
-import java.time.ZonedDateTime;
-import java.util.Objects;
-import java.util.Optional;
 
 @Repository
 public class JdbcOrdersRepositoryImpl implements OrdersRepository {
@@ -51,9 +45,13 @@ public class JdbcOrdersRepositoryImpl implements OrdersRepository {
     public Integer save(Orders orders) {
         orders.setOrderNumber(orderNumber++);
         String sql = "INSERT INTO orders (order_id, order_number, order_datetime) "
-                + "values (:orderId, :orderNumber, :orderDatetime)";
+            + "values (:orderId, :orderNumber, :orderDatetime)";
 
-        SqlParameterSource param = new MapSqlParameterSource("orders", orders);
+        SqlParameterSource param =
+            new MapSqlParameterSource("orders", orders)
+                .addValue("orderId", orders.getOrderId())
+                .addValue("orderNumber", orders.getOrderNumber())
+                .addValue("orderDatetime", orders.getOrderDateTime());
 
         return template.update(sql, param);
     }
@@ -62,8 +60,8 @@ public class JdbcOrdersRepositoryImpl implements OrdersRepository {
     @Override
     public Optional<Orders> findBy(Long orderId) {
         String sql = "SELECT order_id, order_number, order_datetime "
-                + "FROM orders "
-                + "WHERE order_id = :orderId";
+            + "FROM orders "
+            + "WHERE order_id = :orderId";
 
         SqlParameterSource param = new MapSqlParameterSource("orderId", orderId);
 
@@ -72,9 +70,9 @@ public class JdbcOrdersRepositoryImpl implements OrdersRepository {
 
     private RowMapper<Orders> rowMapper() {
         return (rs, rowNum) -> new Orders(
-                rs.getLong("order_id"),
-                rs.getLong("order_number"),
-                rs.getString("order_datetime")
+            rs.getLong("order_id"),
+            rs.getLong("order_number"),
+            rs.getString("order_datetime")
         );
     }
 }
