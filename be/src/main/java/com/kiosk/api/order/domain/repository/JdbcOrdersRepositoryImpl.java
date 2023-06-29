99850ef3ca4745bb44a6b72894ccbf5235586ea8
Sully <2dcoder@naver.com>
2023-06-29 11:13:46 +0900

    ♻️ refactor #50: Order 관련 메서드 분리 후 변수명 수정

--- be/src/main/java/com/kiosk/api/order/domain/repository/JdbcOrdersRepositoryImpl.java
+++ be/src/main/java/com/kiosk/api/order/domain/repository/JdbcOrdersRepositoryImpl.java
@@ -92,16 +92,15 @@ public class JdbcOrdersRepositoryImpl implements OrdersRepository {
 
     @Override
     public Long findSequence(String name) {
-        String query = "SELECT value FROM sequence WHERE name = :name";
+        String query = "SELECT sequence_value FROM sequence_table WHERE sequence_name = :name";
         SqlParameterSource param = new MapSqlParameterSource("name", name);
-        Long sequenceValue = template.queryForObject(query, param, Long.class);
-        return sequenceValue != null ? sequenceValue : null;
+        return template.queryForObject(query, param, Long.class);
     }
 
     @Override
     public void updateSequence(Long value, String name) {
-        String query = "UPDATE sequence SET value = :value WHERE name = :name";
-        SqlParameterSource param = new MapSqlParameterSource("sequence", value);
+        String query = "UPDATE sequence_table SET sequence_value = :value WHERE sequence_name = :name";
+        SqlParameterSource param = new MapSqlParameterSource("value", value);
         template.update(query, param);
     }
 }
